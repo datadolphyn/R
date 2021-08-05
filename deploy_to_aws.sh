96332bf7caa58ebdac13caf577ea1aab0550bf0d
Sujay khandekar <sujaykhandek@gmail.com>
2021-08-05 12:41:38 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
-ssh ubuntu@3.234.99.37 " sudo docker image prune -f 
+ssh ubuntu@3.234.99.37 -i ~/.ssh/id_rsa " sudo docker image prune -f 
         cd /newstorage/users/sujay/R_check 
         sudo docker-compose down
         git fetch origin
